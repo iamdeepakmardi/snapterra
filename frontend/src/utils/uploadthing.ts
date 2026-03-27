39d3a024ce646e0ba85ac33cfa184993e678bd16
Deepak Mardi <deepakmardi22@gmail.com>
2026-03-27 11:07:30 +0530

    added tanstack query + uploadthing cleanup

--- frontend/src/utils/uploadthing.ts
+++ frontend/src/utils/uploadthing.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   generateUploadButton,
   generateUploadDropzone,
@@ -9,9 +10,6 @@ const uploadUrl = import.meta.env.VITE_API_URL
   : "/api/uploadthing";
 
 // Mocking the backend router type to prevent Vercel build from compiling the backend folder
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const UploadButton = generateUploadButton<any>({ url: uploadUrl });
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const UploadDropzone = generateUploadDropzone<any>({ url: uploadUrl });
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const { useUploadThing } = generateReactHelpers<any>({ url: uploadUrl });
